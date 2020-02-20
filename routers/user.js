3bbfc3bd5d453e6076cca94232479784524e5953
Lucas Vinicius <lucas.montenegro@somosphi.com>
2020-02-20 12:55:39 -0300

    ADDED express-validator

--- routers/user.js
+++ routers/user.js
@@ -1,5 +1,7 @@
 var express = require("express");
+const { check, validationResult } = require("express-validator");
 var router = express.Router();
+const userValidator = [check("name").isString(), check("email").isEmail()];
 
 router
   .route("/")
@@ -8,11 +10,17 @@ router
     res.status(200).send("/ GET");
     next();
   })
-  .post((req, res, next) => {
+  .post(userValidator, (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
     const name = req.body.name;
+    const email = req.body.email;
     console.log(name);
+    console.log(email);
     console.log("/ Post");
-    res.status(200).send(`/ POST ${name}`);
+    res.status(200).send(`/ POST ${name}, ${email} `);
     next();
   });
 
